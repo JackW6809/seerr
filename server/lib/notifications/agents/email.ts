ff3a87acb599cac85976eb82323954c1e98e34ea
JackW <53652452+JackW6809@users.noreply.github.com>
2026-02-15 16:07:32 +0000

    Custom notify email (#4)
    * feat(notifications): added a custom notify email field, managed account now get mail
    
    Some people, like me, like to manage peoples Plex account so all they need to worry about is signing
    in with provided username and password. The problem with this and Seerr is that Seerr assumes that
    people want to recieve mail on the email field. This is prefilled and non-changeable if users are
    synced from Plex you cannot change it to the users actual email. In my Plex environment I use my own
    domain for users login but they will never get mail from that, this now resolves that issue.
    
    BREAKING CHANGE: Database Migration
    
    Make null rather than empty string
    
    Remove Debug Code
    
    feat(e-mail notification changes): added a custom notify email field, managed account now get mail
    
    Some people, like me, like to manage peoples Plex account so all they need to worry about is signing
    in with provided username and password. The problem with this and Seerr is that Seerr assumes that
    people want to recieve mail on the email field. This is prefilled and non-changeable if users are
    synced from Plex you cannot change it to the users actual email. In my Plex environment I use my own
    domain for users login but they will never get mail from that, this now resolves that issue.
    
    BREAKING CHANGE: Database Migration
    
    fix(en.json): update Translations
    
    Update translations file due to failed GH Action
    
    * fix(email.ts, seerr-api.yml): fix Bugs in new code
    
    Fixed bugs in new code to ensure notification email is validated, also fixed spelling mistake I made
    in the API
    
    * feat(notifications): updated verbiage, use variable instead of re-computing, update api validation
    
    I have updated the new verbiage I added to ensure it is in the current users context and not the
    admins. Re-used a previously declared variable instead of re-computing it. Updated the acceptable
    format for the API call made.
    
    BREAKING CHANGE: API Updated, Notification Call Updated

--- server/lib/notifications/agents/email.ts
+++ server/lib/notifications/agents/email.ts
@@ -257,8 +257,7 @@ class EmailAgent
               this.buildMessage(
                 type,
                 payload,
-                payload.notifyUser.settings?.notifyEmail ||
-                  payload.notifyUser.email,
+                userEmail,
                 payload.notifyUser.displayName
               )
             );
